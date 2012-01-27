68d039508f87f1b68b545e6e8e8683073b2ce963
Abhishek Bahuguna <abhishek.bahuguna.test@gmail.com>
2012-01-27 16:25:22 +0530

    Midway in capter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+# return title on per page basis
+
+def title
+base_title = "Ruby on Rails Tutorial Sample App"
+if @title == nil
+@title = base_title
+else
+@title = base_title + " | " + @title
+end
+end
 end
